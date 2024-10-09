547763b03b5c6c82bb0fcb98e7c32bd2821f1d57
neetasinghray <neeta.mitra@gmail.com>
2024-10-09 17:45:10 -0400

    Update Diamond.java
    changed the variable name

--- Diamond.java
+++ Diamond.java
@@ -1,79 +1,45 @@
-
 //Diamond.java
-//************************************************************************************************************************************************************************************************* 
-// TO ACCEPT INPUT FROM THE USER IN THE FORM OF AN INTEGER,WHICH CREATES A DIAMOND PATTERN,WHOSE WIDTH ( IN COLUMNS ) AND HEIGHT ( IN ROWS ) ARE DICTATED BY THE USER INPUT.
-//*************************************************************************************************************************************************************************************************
+/* To accept input from the user in the form of an integer,
+  then creates a diamond,whose width(in columns)and height(in rows)
+  are dictated by user input */
+
 import java.util.Scanner;
-public class Diamond 
-{
-  public static void main(String[] args) 
-  {
-    Scanner input = new Scanner(System.in);
-    System.out.print("ENTER THE NUMBER OF ROWS :");
-    int rows = input.nextInt();
 
- //*********************************************************************************************************************************************************************************************************** 
- // PRINT THE TOP HALF OF THE DIAMOND
- //*********************************************************************************************************************************************************************************************************   
- 
- for(int m=1; m<=rows; m++)
- {
-     //INNER LOOP_1 PRINT WHITESPACES INBETWEEN
+public class Diamond {
+  public static void main(String[] args) {
+    Scanner scannerInput = new Scanner(System.in);
+    System.out.print("ENTER A NUMBER :");
+    int userInput = scannerInput.nextInt();
 
-     for( int n = 1 ; n <= rows-m; n++)
-     {
-         System.out.print("   ");
- 
-     } 
+//Prints the top half of the diamond
 
-     //INNER LOOP_2 PRINTS STAR
-
-     for ( int n = 1 ; n <= m * 2 - 1; n++)
-     {
-         System.out.print(" * ");
-     }
-
-     //ENDING LINE AFTER EACH ROW
-
-     System.out.println();
-     
- }
-//************************************************************************************************************************************************************************************************************
-//PRINT THE BOTTOM HALF OF THE DIAMOND
-//************************************************************************************************************************************************************************************************************
-
-for( int m = rows - 1; m > 0; m-- )
-{
-    // INNER LOOP_1 PRINT WHITESPACES INBETWEEN
-
-    for( int n = 1; n <= rows - m; n++ )
-    {
-        System.out.print( "   ");
+  for(int row = 1; row <= userInput; row++){
+    //Inner loop-1 print whitespaces inbetween
+    for(int column = 1 ; column <= userInput-row; column++){
+    System.out.print("   ");
     }
+      //Inner loop-2 prints star
+      for(int column = 1 ; column <= row * 2 - 1; column++){
+      System.out.print(" * ");
+      }
+  //Ending line after each row
+  System.out.println();
+  }
 
-    //INNER LOOP_2 PRINT STAR
+//Prints the bottom half of the diamond
 
-    for(int n =1 ; n <= m * 2 - 1; n++ )
-    {
-        System.out.print(" * ");
-    }
-
-    //ENDING LINE AFTER EACH ROW
-
+   for(int row = userInput - 1; row > 0; row--){
+     //Inner loop-1 prints whitespaces inbetween
+     for(int column = 1; column <= userInput-row; column++){
+     System.out.print( "   ");
+      }
+       //Inner loop-2 prints star
+       for(int column =1 ; column <= row * 2 - 1; column++ ){
+       System.out.print(" * ");
+       }
+    //Ending line after each row
     System.out.println();
-    
-    input.close();
-} 
+    scannerInput.close();
+  }
+ }
 }
-}
-
-
-
-
-
-
-
-
-
-
-
