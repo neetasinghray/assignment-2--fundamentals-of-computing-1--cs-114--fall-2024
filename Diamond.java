eb65c8119c725228b1a6e36b5e070079818dd547
neetasinghray <neeta.mitra@gmail.com>
2024-10-14 16:57:05 -0400

    updated Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -1,25 +1,30 @@
-//Diamond.java
-/* To accept input from the user in the form of an integer,
-  then creates a diamond,whose width(in columns)and height(in rows)
-  are dictated by user input */
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// File : Diamond.java
+//
+// Purpose : To accept input from the user in the form of an integer,
+// then creates a diamond,whose width(in columns)and height(in rows)
+// are dictated by user input
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 import java.util.Scanner;
 
 public class Diamond {
   public static void main(String[] args) {
     Scanner scannerInput = new Scanner(System.in);
-    System.out.print("ENTER A NUMBER :");
+    System.out.print("ENTER A NUMBER  :");
     int userInput = scannerInput.nextInt();
+    int row;      // number of rows
+    int column;   // number of columns
 
 //Prints the top half of the diamond
 
-  for(int row = 1; row <= userInput; row++){
+  for(row = 1; row <= userInput; row++){
     //Inner loop-1 print whitespaces inbetween
-    for(int column = 1 ; column <= userInput-row; column++){
+    for(column = 1 ; column <= userInput-row; column++){
     System.out.print("   ");
     }
       //Inner loop-2 prints star
-      for(int column = 1 ; column <= row * 2 - 1; column++){
+      for(column = 1 ; column <= row * 2 - 1; column++){
       System.out.print(" * ");
       }
   //Ending line after each row
@@ -28,18 +33,18 @@ public class Diamond {
 
 //Prints the bottom half of the diamond
 
-   for(int row = userInput - 1; row > 0; row--){
-     //Inner loop-1 prints whitespaces inbetween
-     for(int column = 1; column <= userInput-row; column++){
-     System.out.print( "   ");
-      }
-       //Inner loop-2 prints star
-       for(int column =1 ; column <= row * 2 - 1; column++ ){
-       System.out.print(" * ");
-       }
-    //Ending line after each row
-    System.out.println();
-    scannerInput.close();
+  for(row = userInput - 1; row > 0; row--){
+    //Inner loop-1 prints whitespaces inbetween
+    for(column = 1; column <= userInput-row; column++){
+    System.out.print( "   ");
+    }
+     //Inner loop-2 prints star
+     for(column =1 ; column <= row * 2 - 1; column++ ){
+     System.out.print(" * ");
+     }
+  //Ending line after each row
+  System.out.println();
+scannerInput.close();
   }
  }
 }
